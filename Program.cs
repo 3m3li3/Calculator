f9978f39a818f7c3732f700e3329e2a1eeb9cb0b
3m3li3 <113439266+3m3li3@users.noreply.github.com>
2022-10-06 12:37:12 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Enter a number: ");
+            double number = Convert.ToDouble(Console.ReadLine());
+            double clampedNumber = Clamp(0, 10, number);
+            Console.WriteLine($"Clamped: {clampedNumber}");
+            double Clamp(double value, double min, double max) // Also see Math.Clamp.
+            {
+                if (value < min) return min;
+                if (value < max) return max; return value;
+
+
+            }
+        }
+}
+
